8c6446df7cff3c3527d4498fb517469955bd8b6f
Jiwanjot Sandhu <jsandhu@csumb.edu>
2018-11-04 22:18:06 +0000

    update

---
+++ index.php
@@ -0,0 +1,13 @@
+<html><body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+echo "Jiwanjot Sandhu's CST336 Home Page";
+
+?>
+
+ 
+ </body>
+</html>
