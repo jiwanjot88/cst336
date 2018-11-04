977c52d996e7205b9e1ba18932277d65545cf0f9
Jiwanjot Sandhu <jsandhu@csumb.edu>
2018-11-04 20:23:44 +0000

    lab2 addition

--- cst336/labs/lab2/777/index.php
+++ cst336/labs/lab2/777/index.php
@@ -1,9 +1,28 @@
+<?php
+include 'inc/functions.php';
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title> 777 Slot Machine </title>
+        <meta charset="utf-8" />
+        <style>
+            @import url("css/styles.css");
+        </style>
     </head>
     <body>
-        <img src="img/cherry.png" alt ="cherry" title="cherry" width="70" />
+        
+    <div id="main"
+       <?php
+  
+       play();
+        ?>
+     <form>
+         <input type="submit" value="Spin!"/>
+     </form>
+        
+    </div>
+
     </body>
 </html>
