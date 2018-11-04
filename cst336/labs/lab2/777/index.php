dd872df46e27c7de8ea2762873df5e66e5ae4126
Jiwanjot Sandhu <jsandhu@csumb.edu>
2018-11-04 20:01:55 +0000

    minor changes

---
+++ cst336/labs/lab2/777/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+    </head>
+    <body>
+        <img src="img/cherry.png" alt ="cherry" title="cherry" width="70" />
+    </body>
+</html>
